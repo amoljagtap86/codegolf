0fcc6eb688dc258d6f504b6b03dfc6d01b0d78ce
aj9682 <amol.jagtap@zsassociates.com>
2015-09-21 13:49:58 +0530

    imperative dds

---
+++ dds.js
@@ -0,0 +1,29 @@
+	(function(){
+		var number = new Number(8675309);
+		var sum = 0;
+		var digitDifferences = [];
+		getDigitDifference(number, digitDifferences);
+		console.log(digitDifferences);
+		
+		for(n in digitDifferences){
+			sum +=	digitDifferences[n];	
+		}
+
+		console.log("sum: " + sum);
+
+		function getDigitDifference(num, differences){
+			differences.push(num);
+			var newNumber ="";
+			var numberStr = num.toString();	
+			if(numberStr.length > 1){
+				for (var i = 0; i < numberStr.length-1; i++) {
+					var num1 = numberStr[i];
+					var num2 = numberStr[i+1];
+					var diff = Math.abs(num1-num2);
+					newNumber += diff;
+					};
+				getDigitDifference(new Number(newNumber), differences);
+				}
+		};
+
+})();
